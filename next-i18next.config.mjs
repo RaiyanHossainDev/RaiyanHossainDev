c93fd906aca7ee237b53c1b8fd2ecc9c1019ff6e
RaiyanHossainDev <astralfog2@gmail.com>
2026-02-17 22:21:47 +0600

    Responsive done without Banner

--- next-i18next.config.mjs
+++
@@ -1,5 +0,0 @@
-export const i18n = {
-  locales: ['en', 'bn'], // List of supported locales
-  defaultLocale: 'en', // Default locale
-  localeDetection: true, // Enable automatic locale detection
-};
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-import { i18n } from './next-i18next.config.mjs';
 
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  i18n,
 };
 
 export default nextConfig;
